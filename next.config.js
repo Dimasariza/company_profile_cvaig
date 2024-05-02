09c8997069af7a1424878456a9c77bd554bd1011
dimasariza <roganda.sihombing11@gmail.com>
2024-05-02 19:52:43 +0700

    change next config

--- next.config.js
+++ next.config.js
@@ -1,4 +1,4 @@
-const { withNextVideo } = require('next-video/process')
+// const { withNextVideo } = require('next-video/process')
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
@@ -19,4 +19,4 @@ const nextConfig = {
   },
 };
 
-module.exports =  withNextVideo(nextConfig);
+module.exports =  nextConfig;
